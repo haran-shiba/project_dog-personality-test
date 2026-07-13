828f0db62ff518c946c6b082d13b8f2d5107429c
兼松つばさ <yasu.0127abc@icloud.com>
2026-07-13 21:34:37 +0900

    スタートボタンで画面切り替え

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 console.log("JavaScript connected");
+const topScreen = document.getElementById("top-screen");
+const questionScreen = document.getElementById("question-screen");
+const startButton = document.getElementById("start-btn");
+
+startButton.addEventListener("click", function(){
+    topScreen.style.display = "none";
+    questionScreen.style.display = "flex";
+});
